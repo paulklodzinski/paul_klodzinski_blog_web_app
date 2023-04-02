cd380dc501547cdc6313bce579da5bd80b36dbf5
paulklodzinski <paul.klodzinski@gmail.com>
2023-04-02 15:00:01 -0500

    Update Blog Title

--- main.py
+++ main.py
@@ -92,8 +92,6 @@ with app.app_context():
         text = db.Column(db.Text, nullable=False)
         blog_post_id = db.Column(db.Integer, db.ForeignKey('blog_posts.id'))
 
-    # db.create_all()
-
 
 @app.route('/')
 def get_all_posts():
